2f793b7923f4a0b10e87b316862a9cd49394acd7
SerhiiWD <147054860+SerhiiWD@users.noreply.github.com>
2023-10-18 15:35:32 +0300

    up

--- job-form-and-payment-test-62.js
+++ job-form-and-payment-test-62.js
@@ -35,7 +35,7 @@ let elements;
 
 (async function() {
   await fetch("https://dev--get-prices--sarimpro.autocode.dev/")
-  .then(response => response.json())
+  .then(response => console.log(prices)) //response.json())
   .then(prices => console.log(prices));
 })();
 
